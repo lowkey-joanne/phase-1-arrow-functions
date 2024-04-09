0320eea1ae6c8eb7a8116986d974429e6ab639c0
joanne <joannembula33@gmail.com>
2024-04-09 13:35:06 +0300

    completed lab

---
+++ index.js
@@ -0,0 +1,14 @@
+function show(value){
+    console.log(value);
+}
+
+function divide(value1, value2){
+    return value1 / value2;
+}
+
+let  square = value => value*value;
+
+let add = (value3,value4) => value3 + value4;
+
+
+
